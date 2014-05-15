c54e346b4c59299c3b70ecd3faf2b1167cb61c78
Matt <mheavens7028@sohamcollege.org.uk>
2014-05-15 09:41:56 +0100

    Update diceRoller.py
    I used a while loop to keep rolling if the user chooses.

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,9 @@
-#Coin flip program
+#Dice roll program
 #Describe the purpose of this program here.
 
-inport random;time
+import random
+import time
+question = "y"
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -11,25 +13,27 @@ s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
-
-
-def show_dice(roll)
-    if roll = 1:
+    print("rolling.....")
+    roll = random.randint(1,6)
+    return roll
+    
+def show_dice(roll):
+    if roll == 1:
         print(S1)
-    else roll = 2
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll == 3:
         print(s3)
-    else roll = 4
+    elif roll == 4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    else:
         print(s6)
 
-roll
-time.sleep(1)
-show_dice(roll)
+while question == "y":    #Keeps rolling until the user answers "n"
+    myroll = roll()
+    time.sleep(1)
+    show_dice(myroll)
+    question = input("Roll again? (y/n) >>> ")
 
